c8e372e774d8b3127ad04e5b221efa64c81d7e0e
roytiburcio <Roy.Tiburcio@gmail.com>
2015-05-24 15:53:33 -0700

    Initialize

---
+++ src/main/java/tiburcio/client/Messages.java
@@ -0,0 +1,10 @@
+package tiburcio.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
