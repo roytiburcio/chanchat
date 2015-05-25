14a52ee276f2bfca45edc74f742ba304b5009041
roytiburcio <Roy.Tiburcio@gmail.com>
2015-05-24 19:08:14 -0700

    stuff is broken... why

--- src/main/java/tiburcio/client/Messages.java
+++ src/main/java/tiburcio/client/Messages.java
@@ -5,6 +5,6 @@ public interface Messages extends com.google.gwt.i18n.client.Messages {
   @DefaultMessage("Enter your name")
   String nameField();
 
-  @DefaultMessage("Send")
+  @DefaultMessage("Sendorito")
   String sendButton();
 }
