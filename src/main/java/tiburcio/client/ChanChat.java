9ed25530d38e9f407624c90c510d7f798d87d62d
roytiburcio <Roy.Tiburcio@gmail.com>
2015-05-25 10:02:24 -0700

    Added basic elements

--- src/main/java/tiburcio/client/ChanChat.java
+++ src/main/java/tiburcio/client/ChanChat.java
@@ -2,12 +2,19 @@ package tiburcio.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class ChanChat extends Composite {
   interface Binder extends UiBinder<HTMLPanel, ChanChat> {}
   final Binder binder = GWT.create(Binder.class);
+  @UiField Button sendButton;
+  @UiField TextArea chatBox;
+  @UiField TextBox inputBox;
   
   public ChanChat() {
     initWidget(binder.createAndBindUi(this));
