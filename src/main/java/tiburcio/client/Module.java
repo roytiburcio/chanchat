79858299b2c500bafe024e169b042a5fc3c169b6
roytiburcio <Roy.Tiburcio@gmail.com>
2015-05-25 09:29:35 -0700

    Fixed ui.xml stuff

--- src/main/java/tiburcio/client/Module.java
+++ src/main/java/tiburcio/client/Module.java
@@ -1,7 +1,6 @@
 package tiburcio.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -9,12 +8,11 @@ import com.google.gwt.user.client.ui.RootPanel;
  */
 public class Module implements EntryPoint {
 
-
   /**
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    HomePage homePage = GWT.create(HomePage.class); 
-    RootPanel.getBodyElement().appendChild(homePage.getElement());
+    HomePage homePage = new HomePage();
+    RootPanel.get().add(homePage);
   }
 }
